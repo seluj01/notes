5e462793f8804e77c916e52031a6cb0e345d7ffc
Jules Matz <jules.matz@posteo.net>
2025-09-02 18:13:18 +0200

    Deployed 7e99366 with MkDocs version: 1.6.1

--- Estimation/gaussian2Dplot.jl
+++ Estimation/gaussian2Dplot.jl
@@ -2,11 +2,12 @@ using Distributions, Plots, LinearAlgebra, Random
 
 Random.seed!(1234) # for reproducibility
 
-μ = [1.0; 2.0]  # Mean vector
-Σ = [2.0 1.0; 1.0 1.5]  # Covariance matrix
+μ = [1.0; 2.0]         # Mean
+Σ = [2.0 1.0; 1.0 1.5] # Covariance
 dist = MvNormal(μ, Σ)
 
 # Confidence ellipse
+# Compute square root of Σ
 Λ,Q = eigen(Σ) # Σ = QΛ/Q
 Σsqrt = Q*diagm(sqrt.(Λ))
 
@@ -21,11 +22,12 @@ ell_1σ = μ .+ Σsqrt * circle
 n_samples = 100
 samples = rand(dist, n_samples)
 
-# Plot the samples and the ellipse
+# Plot samples and ellipse
 plot(title="Bivariate Gaussian Distribution", aspect_ratio=:equal)
 scatter!(samples[1,:], samples[2,:], label="Samples", ms=2)
 plot!(ell_1σ[1,:], ell_1σ[2,:], label="1-σ ellipse", color=:red)
-xlabel!("X")
-ylabel!("Y")
+xlabel!("X_1")
+ylabel!("X_2")
+# save image
 path_save = dirname(@__FILE__()) * "/img/gaussian2Dplot.png"
 savefig(path_save)
